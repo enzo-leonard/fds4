d813e8b381fff459de8d043f4a444abbde5efd5d
enzo leonard <enzoleonard96@gmail.com>
2020-09-15 00:53:35 +0200

    update

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 
 module Fds4
   class Application < Rails::Application
