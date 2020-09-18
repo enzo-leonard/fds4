550f9480e9070627f07fad44e74a786ff700cf09
enzo leonard <enzoleonard96@gmail.com>
2020-09-18 19:59:12 +0200

    update

--- app/javascript/channels/modal.js
+++ app/javascript/channels/modal.js
@@ -1,33 +1,50 @@
 const initModal = () => {
 
     const toggleBtn = document.querySelectorAll('[data-target]')
-    const blurBtn = document.querySelectorAll('.background-modal, .close-btn')
-    const modals = document.querySelectorAll('.modal-container')
+    const blurBtn = document.querySelectorAll('.background-modal, .close-btn, .close-btn-black')
+  
+    const modals = document.querySelectorAll('.modal-container, .modal-container-mobile')
     const navBar = document.querySelector('nav')
     const underBar = document.querySelector('.under-bar')
+    const mobile = document.querySelector('.mobile')
+
  
 
     navBar.addEventListener('click', () => { underBar.classList.toggle('active')})
 
-    toggleBtn.forEach((btn) => {
+        // some code..
        
-        btn.addEventListener('click', () => {
-            console.log('hey')
-            const target = btn.dataset.target 
-            console.log(target)
-            document.querySelector(`${target}`).classList.toggle('hidden')
-            blurBtn[0].classList.remove("hidden")
-        })
-    })
+         console.log('MOBILE')
 
+        toggleBtn.forEach((btn) => {
+            
+        
+            btn.addEventListener('click', () => {
+                console.log(mobile.style.display)
+
+                const target = btn.dataset.target 
+               
+                document.querySelector(`${target}`).classList.toggle('hidden')
+                
+                if (btn.dataset.type == "close"){
+                    blurBtn[0].classList.add("hidden")
+                }
+                else{
+                    blurBtn[0].classList.remove("hidden")
+                }
+              
+            })
+        })
+
+        
+        blurBtn.forEach((btn) => {
+            btn.addEventListener('click', () => {
+            modals.forEach((modal) => {modal.classList.add('hidden')})
+            blurBtn[0].classList.add("hidden")
+            })
+
+        })
     
-    blurBtn.forEach((btn) => {
-        btn.addEventListener('click', () => {
-        modals.forEach((modal) => {modal.classList.add('hidden')})
-        blurBtn[0].classList.add("hidden")
-        })
-
-    })
 
 }
 
