8e419d8117ddd90e2153d5ffe9ebf32d7546338f
enzo leonard <enzoleonard96@gmail.com>
2020-09-23 08:41:24 +0200

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def default_url_options
+    { host: "https://fete-de-la-science.herokuapp.com/" }
+  end
+
+  #https://www.fetedelascience-aura-numerique.com/
 end
